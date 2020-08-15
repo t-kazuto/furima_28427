8dd5f78694aae51acf8760f21dc219515f20b9a8
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-15 18:00:06 +0900

    Userテーブル修正

--- db/migrate/20200814090145_devise_create_users.rb
+++ db/migrate/20200814090145_devise_create_users.rb
@@ -3,16 +3,23 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      ## Database authenticatable
       t.string :nickname, null: false
-      t.string :email, null: false
-      t.string :password, null: false
-      t.string :password_confirmation, null: false
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
       t.string :family_name, null: false
       t.string :first_name, null: false
       t.string :family_name_katakana, null: false
       t.string :first_name_katakana, null: false
       t.date :birthday, null: false
       t.timestamps null: false
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+      
+      ## Rememberable
+      t.datetime :remember_created_at
     end
 
     add_index :users, :email,                unique: true
