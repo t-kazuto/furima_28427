63f1e3a3460434ef3379bf2dc02b8b969876c3dd
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-17 14:17:19 +0900

    usersコントローラールーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
 end
