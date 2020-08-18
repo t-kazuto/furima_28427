2e6766f89520c285e8bed1afb533697be78c3ef6
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-18 15:28:02 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   
+   ##ニックネーム
+   validates :nickname, presence: true
+
+   #パスワード
+   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
    with_options presence: true do
-      ## ユーザー情報
-      validates :email, uniqueness: true
-      validates :email, inclusion: { in: %w("@") }
-      validates :password, format: { with: /[a-z\d]{6,}/i }
-      validates :password_confirmation, presence: true
-      ##本人情報確認
-      validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-      validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-      validates :family_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
-      validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   ##本人情報確認
+   validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   validates :family_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   validates :first_name_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
+   ##生年月日
+   validates :birthday, presence: true
    end
 
 end
