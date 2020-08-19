0c46d199a1365b556da91a3f45a0ef768d03ca3c
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-19 23:44:43 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :price)
+  end
+
 end
