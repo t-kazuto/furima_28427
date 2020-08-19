dac88e30e53413c80f00b3964d669f79d6605659
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-19 15:26:15 +0900

    モデル単体テストコード_修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -65,24 +65,75 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
 
-    it 'passwordが6文字以上かつ半角英数字であれば登録できること' do
+    it 'passwordが6文字以上かつ半角英数字であれば登録できる' do
       @user.password = 'test1234'
       @user.password_confirmation = 'test1234'
       expect(@user).to be_valid
     end
 
-    it 'passwordが5文字以下であれば登録できないこと' do
+    it 'passwordが5文字以下であれば登録できない' do
       @user.password = '12345'
       @user.password_confirmation = '12345'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
-    it '重複したemailが存在する場合登録できないこと' do
+    it 'パスワードが英数だけの場合登録できない' do
+      @user.password = 'aaaaaa'
+      @user.password_confirmation = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is invalid')
+    end
+
+    it '重複したemailが存在する場合登録できない' do
       @user.save
       another_user = FactoryBot.build(:user, email: @user.email)
       another_user.valid?
       expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
+    it 'family_nameが英数字でなければ登録できる' do
+      @user.family_name = '阿'
+      expect(@user).to be_valid
+    end
+
+    it 'family_nameが英数字である場合登録できない' do
+      @user.family_name = 'a'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Family name is invalid')
+    end
+
+    it 'first_nameが英数字でなければ登録できる' do
+      @user.first_name = '阿'
+      expect(@user).to be_valid
+    end
+
+    it 'first_nameが英数字である場合登録できない' do
+      @user.first_name = 'a'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name is invalid')
+    end
+
+    it 'family_name_katakanaがカタカナであれば登録できる' do
+      @user.family_name_katakana = 'ア'
+      expect(@user).to be_valid
+    end
+
+    it 'family_name_katakanaがカタカナでない場合登録できない' do
+      @user.family_name_katakana = 'あ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Family name katakana is invalid')
+    end
+
+    it 'first_name_katakanaがカタカナであれば登録できる' do
+      @user.first_name_katakana = 'ア'
+      expect(@user).to be_valid
+    end
+
+    it 'first_name_katakanaがカタカナでない場合登録できない' do
+      @user.first_name_katakana = 'あ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('First name katakana is invalid')
     end
   end
 end
