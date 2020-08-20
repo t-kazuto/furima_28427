93d63c2bab28bc70c9e09162d60cfaac0446a630
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-20 11:13:09 +0900

    itemsテーブル修正

--- db/migrate/20200819085646_create_items.rb
+++ db/migrate/20200819085646_create_items.rb
@@ -3,6 +3,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :name, null: false
       t.string :explanation, null: false
+      t.integer :category, null: false
+      t.integer :condition, null: false
+      t.integer :shipping_charges, null: false
+      t.integer :shipping_area, null: false
+      t.integer :shipping_days, null: false
       t.integer :price, null: false
       t.timestamps
     end
