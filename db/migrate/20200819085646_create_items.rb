fa772882becc71a5f08f8c7993f2f3efeeebe496
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-20 20:29:15 +0900

    itemsテーブルカラム修正

--- db/migrate/20200819085646_create_items.rb
+++ db/migrate/20200819085646_create_items.rb
@@ -3,11 +3,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :name, null: false
       t.string :explanation, null: false
-      t.integer :category, null: false
-      t.integer :condition, null: false
-      t.integer :shipping_charges, null: false
-      t.integer :shipping_area, null: false
-      t.integer :shipping_days, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :shipping_charges_id, null: false
+      t.integer :shipping_area_id, null: false
+      t.integer :shipping_days_id, null: false
       t.integer :price, null: false
       t.timestamps
     end
