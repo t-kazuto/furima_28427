e7db9a39512799d3ebec8456ff7df5447654d2da
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-20 14:55:17 +0900

    配送料の負担・発送元の地域・発送までの日数実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :shipping_days
 
   validates :category, numericality: { other_than: 1 } 
 end
