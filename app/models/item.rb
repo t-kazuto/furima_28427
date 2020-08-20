524ececb7c5701f5c16cd63188b4223c8614f19e
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-20 14:12:14 +0900

    商品の状態実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
 
   validates :category, numericality: { other_than: 1 } 
 end
