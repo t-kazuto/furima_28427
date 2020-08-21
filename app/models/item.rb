2cc6ad4913b116df3b49e564f70569937aa82a2e
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-21 12:54:30 +0900

    Activehashのモデルの関係性表記修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,17 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_charges
+  belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_area
-  belongs_to_active_hash :shipping_days
-  
+  belongs_to_active_hash :shipping_day
 
-  with_options presence: true do
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :condition_id, numericality: { other_than: 1 } 
-    validates :shipping_charges_id, numericality: { other_than: 1 } 
-    validates :shipping_area_id, numericality: { other_than: 1 } 
-    validates :shipping_days_id, numericality: { other_than: 1 } 
-    validates :price, numericality: { in: 300..9999999 }
-  end
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :shipping_charges_id, numericality: { other_than: 1 } 
+  validates :shipping_area_id, numericality: { other_than: 1 } 
+  validates :shipping_days_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { in: 300..9999999 }
+  
 end
