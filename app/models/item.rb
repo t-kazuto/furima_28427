889b83ba8ad6f65f000e7251f3d6f44aaf09a85f
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-22 17:38:06 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,22 @@
 class Item < ApplicationRecord
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :shipping_area
   belongs_to_active_hash :shipping_day
-  has_one_attached :image
 
   with_options presence: true do
-  validates :image, :name, :explanation, :category_id, :condition_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id, :price
+    validates :image
+    validates :name
+    validates :explanation
+    validates :category_id, numericality: { other_than: 1, message: 'Select' }
+    validates :condition_id, numericality: { other_than: 1, message: 'Select' }
+    validates :shipping_charges_id, numericality: { other_than: 1, message: "Select" }
+    validates :shipping_area_id, numericality: { other_than: 1, message: "Select" }
+    validates :shipping_days_id, numericality: { other_than: 1, message: "Select" }
+    validates :price, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Harf-width number" }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   end
-
-  with_options numericality: { other_than: 1 }  do
-  validates :category_id, :condition_id, :shipping_charges_id, :shipping_area_id, :shipping_days_id
-  end
-
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
