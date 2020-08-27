b9a7fc513dae346c68b523aaac75699d68314d3b
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-27 11:14:33 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  root to: 'orders#index'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
-  resources :items
-  resources :orders, only:[:create]
+  resources :items do
+    resources :orders, only: :index
+  end
 end
