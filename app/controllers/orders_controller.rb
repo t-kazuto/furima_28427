a488c22e643a84e7b2b9b7715ee4a28d65b2c392
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-27 21:04:55 +0900

    クレジットカード決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,14 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(price: order_params[:price])
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
@@ -17,5 +25,18 @@ class OrdersController < ApplicationController
     else
        render :index
     end
+  end
+
+  def order_params
+    params.permit(:price, :token)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 end
