918c50fbecc10445c525582891061bbe99a846e6
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-27 15:20:25 +0900

    出品者がURLを直接入力した場合の遷移の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+  before_action :exhibitor_move_to_index, only: [:index]
 
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
   end
-  
+
+  private
+  def exhibitor_move_to_index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id
+       redirect_to root_path
+    else
+       render :index
+    end
+  end
 end
