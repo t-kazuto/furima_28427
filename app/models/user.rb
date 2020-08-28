791afda53a0d15601488530a1c87e4553236c415
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-28 11:58:02 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
+  has_many :user_items
+  has_many :items, through: :user_items
 
   zenkaku = /\A[ぁ-んァ-ン一-龥]/
   zenkakukatakana = /\A[ァ-ヶー－]+\z/
--- app/models/user_item.rb
+++ app/models/user_item.rb
@@ -1,2 +1,5 @@
 class UserItem < ApplicationRecord
+  belongs_to :uer
+  belongs_to :item
+  has one :address
 end
