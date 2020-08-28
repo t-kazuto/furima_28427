40da10ecf00d5c1321ad43ad83e6941db5041b25
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-28 14:12:53 +0900

    addressesテーブル再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_131931) do
+ActiveRecord::Schema.define(version: 2020_08_28_050812) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_131931) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
-    t.integer "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
     t.string "building"
@@ -87,19 +87,8 @@ ActiveRecord::Schema.define(version: 2020_08_27_131931) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "users_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_users_items_on_item_id"
-    t.index ["user_id"], name: "index_users_items_on_user_id"
-  end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "user_items"
   add_foreign_key "user_items", "items"
   add_foreign_key "user_items", "users"
-  add_foreign_key "users_items", "items"
-  add_foreign_key "users_items", "users"
 end
