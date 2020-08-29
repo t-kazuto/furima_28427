6efe7bd46bb7567b098cfa5395a4e89d4da8757f
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-29 16:47:37 +0900

    price取得の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = UserItemAddress.new(order_params)
     if @order.valid?
       pay_item
       @order.save
@@ -33,10 +33,10 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    #binding.pry
+    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price],  # 商品の値段
+      amount: @item.price,  # 商品の値段
       card: order_params[:token],    # カードトークン
       currency:'jpy'                 # 通貨の種類(日本円)
     )
