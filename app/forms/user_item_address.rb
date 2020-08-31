095c29dfd35f123e3f70382fd0638c7bb83b4e3e
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-08-31 15:11:33 +0900

    バリデーション の内容を変数に代入

--- app/forms/user_item_address.rb
+++ app/forms/user_item_address.rb
@@ -2,13 +2,16 @@ class UserItemAddress
   include ActiveModel::Model
   attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
 
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/ 
+  PHONE_NUMBER_REGEX = /\A\d{11}\z/
+
   with_options presence: true do
     validates :token
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX }
   end
 
   def save
